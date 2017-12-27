6e4aceb93da9247ff6084e08a2b9c23d012c0592
Harshal Jarikote <jarikote.harshal@gmail.com>
2017-12-27 14:58:55 +0530

    Update main.py

--- functions/dependency/main.py
+++ functions/dependency/main.py
@@ -22,6 +22,6 @@ def handle(event, context):
     logger.info("%s", raw)
     result = raw.json()
 
-    logger.info("Lambda IP: %s", result['ip'])
+    logger.info("DEV-Lambda IP: %s", result['ip'])
 
     return event
